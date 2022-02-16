ad588799b1675276af7262b28e19efa74142ec3b
Denis <jujojq@gmail.com>
2022-02-16 09:38:09 +0300

    ls 1

---
+++ notes.controllers.js
@@ -0,0 +1,48 @@
+const fs = require("fs/promises");
+const path = require("path");
+const chalk = require("chalk");
+
+const notesPath = path.join(__dirname, "db.json");
+
+async function addNote(title) {
+  //   const notes = require("./db.json");
+  //   const notes = Buffer.from(buffer).toString("utf-8");
+
+  const notes = await getNotes();
+
+  const note = {
+    title,
+    id: Date.now().toString(),
+  };
+
+  notes.push(note);
+
+  await fs.writeFile(notesPath, JSON.stringify(notes));
+  console.log(chalk.bgGreen("Note was added"));
+}
+
+async function getNotes() {
+  const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
+  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
+}
+
+async function printNotes() {
+  const notes = await getNotes();
+  console.log(chalk.bgBlue("Here is the list of notes"));
+  notes.forEach((note) => {
+    console.log(chalk.blue(note.id, note.title));
+  });
+}
+
+async function removeNote(id) {
+  const notes = await getNotes();
+  const updateNotes = notes.filter((n) => n.id !== id);
+  await fs.writeFile(notesPath, JSON.stringify(updateNotes));
+  console.log(chalk.bgRed("Note was removed"));
+}
+
+module.exports = {
+  addNote,
+  printNotes,
+  removeNote,
+};
